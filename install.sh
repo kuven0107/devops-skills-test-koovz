8376bf0e944bc2f10c29a6c28f62b1a964fe5e15
kuven0107 <84434339+kuven0107@users.noreply.github.com>
2021-05-20 00:55:29 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -8,22 +8,25 @@ else
         chmod +x get-docker.sh
         sh get-docker.sh
         systemctl start docker
+	dnf update docker-ce
 fi
 
 
 ##This it for container, checks if container exist and if so Deletes it. mount /var/lib/mysql on host and also /BATCAVE on host
 
 if
-        docker ps -q -f name=alfred ; then docker container stop alfred; docker container rm alfred; docker container run -p 3306:3306 -e  MYSQL_ROOT_PASSWORD=password --mount type=bind,src=/var/lib/mysql,dst=/var/lib/mysql --mount type=bind,source=/root/BATCAVE,target=/root/BATCAVE --name alfred -d kuven0107/mariadb_xtra:testing
+        docker ps -q -f name=alfred ; then docker container stop alfred; docker container rm alfred; docker container run -p 3306:3306 -e  MYSQL_ROOT_PASSWORD=Pa55w0rd --mount type=bind,src=/var/lib/mysql,dst=/var/lib/mysql --mount type=bind,source=/root/BATCAVE,target=/root/BATCAVE --name alfred -d kuven0107/mariadb_xtra:testing
 fi
 
+#Delay for DB to Start up properly
+sleep 15
 
 #Drop DB if exists
-docker exec -it alfred mysql -uroot -ppassword -e "DROP DATABASE IF EXISTS wayneindustries"
+docker exec -it alfred mysql -uroot -pPa55w0rd -e "DROP DATABASE IF EXISTS wayneindustries"
 #Add DB 
-docker exec -it alfred mysql -uroot -ppassword -e "CREATE DATABASE wayneindustries"
+docker exec -it alfred mysql -uroot -pPa55w0rd -e "CREATE DATABASE wayneindustries"
 #Add table to DB
-docker exec -it alfred mysql -uroot -ppassword -D wayneindustries -e "CREATE TABLE fox(ID varchar(25),Name varchar(25))"
+docker exec -it alfred mysql -uroot -pPa55w0rd -D wayneindustries -e "CREATE TABLE fox(ID varchar(25),Name varchar(25))"
 #Insert values 
-docker exec -it alfred mysql -uroot -ppassword -D wayneindustries -e "INSERT INTO fox (ID , Name) VALUES ( '50' , 'batmobile')"
+docker exec -it alfred mysql -uroot -pPa55w0rd -D wayneindustries -e "INSERT INTO fox (ID , Name) VALUES ( '50' , 'batmobile')"
 
